50bf86226f2c7e5aeec4f1406fe9f2a0ff71c8c8
Omar Baba <omar2xba@gmail.com>
2023-10-27 16:21:07 -0400

    better bokeh

--- sliders.py
+++ sliders.py
@@ -1,83 +1,14 @@
-""" Present an interactive function explorer with slider widgets.
+from bokeh.plotting import figure, show
 
-Scrub the sliders to change the properties of the ``sin`` curve, or
-type into the title text box to update the title of the plot.
+# prepare some data
+x = [1, 2, 3, 4, 5]
+y = [6, 7, 2, 4, 5]
 
-Use the ``bokeh serve`` command to run the example by executing:
+# create a new plot with a title and axis labels
+p = figure(title="Simple line example", x_axis_label="x", y_axis_label="y")
 
-    bokeh serve sliders.py
+# add a line renderer with legend and line thickness
+p.line(x, y, legend_label="Temp.", line_width=2)
 
-at your command prompt. Then navigate to the URL
-
-    http://localhost:5006/sliders
-
-in your browser.
-
-"""
-import numpy as np
-
-from bokeh.io import curdoc
-from bokeh.layouts import column, row
-from bokeh.models import ColumnDataSource, Slider, TextInput
-from bokeh.plotting import figure
-
-# Set up data
-N = 200
-x = np.linspace(0, 4 * np.pi, N)
-y = np.sin(x)
-source = ColumnDataSource(data=dict(x=x, y=y))
-
-
-# Set up plot
-plot = figure(
-    plot_height=400,
-    plot_width=400,
-    title="my sine wave",
-    tools="crosshair,pan,reset,save,wheel_zoom",
-    x_range=[0, 4 * np.pi],
-    y_range=[-2.5, 2.5],
-)
-
-plot.line("x", "y", source=source, line_width=3, line_alpha=0.6)
-
-
-# Set up widgets
-text = TextInput(title="title", value="my sine wave")
-offset = Slider(title="offset", value=0.0, start=-5.0, end=5.0, step=0.1)
-amplitude = Slider(title="amplitude", value=1.0, start=-5.0, end=5.0, step=0.1)
-phase = Slider(title="phase", value=0.0, start=0.0, end=2 * np.pi)
-freq = Slider(title="frequency", value=1.0, start=0.1, end=5.1, step=0.1)
-
-
-# Set up callbacks
-def update_title(attrname, old, new):
-    plot.title.text = text.value
-
-
-text.on_change("value", update_title)
-
-
-def update_data(attrname, old, new):
-
-    # Get the current slider values
-    a = amplitude.value
-    b = offset.value
-    w = phase.value
-    k = freq.value
-
-    # Generate the new curve
-    x = np.linspace(0, 4 * np.pi, N)
-    y = a * np.sin(k * x + w) + b
-
-    source.data = dict(x=x, y=y)
-
-
-for w in [offset, amplitude, phase, freq]:
-    w.on_change("value", update_data)
-
-
-# Set up layouts and add to document
-inputs = column(text, offset, amplitude, phase, freq)
-
-curdoc().add_root(row(inputs, plot, width=800))
-curdoc().title = "Sliders"
+# show the results
+show(p)
