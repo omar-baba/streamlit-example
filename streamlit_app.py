f9be78fff74e50446a7729a6179415e98d96872b
Omar Baba <omar+1@posit.co>
2024-05-20 18:53:42 +0100

    Update streamlit_app.py
    add MY_VAR

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,8 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+st.text(os.environ)
+st.text(os.environ.get('MY_VAR', 'not found'))
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
